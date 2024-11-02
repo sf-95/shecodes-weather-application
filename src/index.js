466efe2af3187fd2390d60bd82e4a8c6ef0b8068
sf-95 <sfarch95@gmail.com>
2024-11-02 13:33:57 +0000

    Injected live weather icon

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ function refreshWeather(response) {
   let wind = response.data.wind.speed;
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  console.log(response.data);
-
+  iconElement.innerHTML = `  <img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   timeElement.innerHTML = formatDate(date);
   windElement.innerHTML = wind;
   humidityElement.innerHTML = humidity;
