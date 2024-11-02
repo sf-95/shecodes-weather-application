be7337d5d2779a2471eeef0b284eb4327db2d000
sf-95 <sfarch95@gmail.com>
2024-11-02 13:08:06 +0000

    Injecting more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,45 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#description");
+  let condition = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  console.log(response.data);
+
+  timeElement.innerHTML = formatDate(date);
+  windElement.innerHTML = wind;
+  humidityElement.innerHTML = humidity;
+  conditionElement.innerHTML = condition;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
